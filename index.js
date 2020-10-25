e14561b1c110fd8cbb72d5ba6cbdfb5fadb2b1c8
CoffeeYes <culinecro@gmail.com>
2020-10-25 12:49:19 +0100

    add navigation

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
+import 'react-native-gesture-handler';
 /**
  * @format
  */
-
+import React from 'react';
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import { NavigationContainer } from '@react-navigation/native';
 
-AppRegistry.registerComponent(appName, () => App);
+const Index = () => {
+  return (
+    <NavigationContainer>
+      <App/>
+    </NavigationContainer>
+  )
+}
+
+AppRegistry.registerComponent(appName, () => Index);
