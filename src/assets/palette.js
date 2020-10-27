3d4b0b2d5d46a7b86aa013ad0c36b554628ebfcd
CoffeeYes <culinecro@gmail.com>
2020-10-27 18:27:00 +0100

    add color palette file, give splash very basic structure

---
+++ src/assets/palette.js
@@ -0,0 +1,5 @@
+//https://coolors.co/eeebd0-ebb3a9-e87ea1-e86252-ee2677
+export default palette = {
+  "primary" : "#E86252",
+  "secondary" : "#EBB3A9",
+}
--- src/views/Splash.js
+++ src/views/Splash.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import { Text, Button } from 'react-native';
+import { View, Text, Button } from 'react-native';
+import palette from '../assets/palette.js'
 
 const Splash = ({ navigation }) => {
   return (
-    <>
-    <Text>Splash</Text>
-    <Button title="contacts" onPress={() => navigation.navigate('contacts')}/>
-    </>
+    <View style={{
+      alignItems : "center",
+      justifyContent : "center",
+      backgroundColor : "#e05e3f",
+      flex : 1}}>
+      <Text style={{color : "white",marginBottom : 20}}>
+        Welcome to Herd, the peer-to-peer messaging app!
+      </Text>
+      <Button title="Get Started" color={palette.secondary}/>
+    </View>
   )
 }
 
