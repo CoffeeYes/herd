b77dd2074cedb49688b9c553a54491e3cca4fd7c
CoffeeYes <culinecro@gmail.com>
2020-10-29 15:30:58 +0100

    init cryptography library

---
+++ @react-native-cryptography/index.js
@@ -0,0 +1,6 @@
+
+import { NativeModules } from 'react-native';
+
+const { RNReactNativeCryptography } = NativeModules;
+
+export default RNReactNativeCryptography;
---
+++ @react-native-cryptography/ios/RNReactNativeCryptography.m
@@ -0,0 +1,13 @@
+
+#import "RNReactNativeCryptography.h"
+
+@implementation RNReactNativeCryptography
+
+- (dispatch_queue_t)methodQueue
+{
+    return dispatch_get_main_queue();
+}
+RCT_EXPORT_MODULE()
+
+@end
+  
