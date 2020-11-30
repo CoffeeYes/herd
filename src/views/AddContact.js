355842a044c7e6956872ec9403dabeb82de003d0
CoffeeYes <culinecro@gmail.com>
2020-11-30 18:18:59 +0100

    create basic addContact layout

--- src/views/AddContact.js
+++ src/views/AddContact.js
@@ -1,10 +1,29 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 
 const AddContact = () => {
   return (
-    <Text>AddContact</Text>
+    <>
+    <View style={styles.header}>
+      <Text style={{color : "white"}}>Add Contact</Text>
+    </View>
+    <View style={styles.main}>
+      <Text>Enable NFC and place your phones next to each other!</Text>
+    </View>
+    </>
   )
+}
+
+const styles = {
+  header : {
+    backgroundColor : "#E86252",
+    padding : 15
+  },
+  main : {
+    alignSelf : "center",
+    marginTop : "auto",
+    marginBottom : "auto"
+  }
 }
 
 export default AddContact;
