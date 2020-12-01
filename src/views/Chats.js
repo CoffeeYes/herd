14ba20c8101deb514740a8c4b294441fa970100e
CoffeeYes <culinecro@gmail.com>
2020-12-01 17:52:53 +0100

    add custom header to chats page

--- src/views/Chats.js
+++ src/views/Chats.js
@@ -5,6 +5,11 @@ const Chats = ({ navigation }) => {
   const [chats,setChats] = useState([{name : "Test1"},{name : "Test2"},{name : "Test3"}]);
   return (
     <>
+    <View style={styles.header}>
+      <Text style={{color : "white", fontSize : 18}}>
+        Chats
+      </Text>
+    </View>
     {chats.map( (chat, index) =>
       <TouchableOpacity
       key={index}
@@ -26,6 +31,14 @@ const styles = {
     alignItems : "space-between",
     borderBottomWidth : 1,
     borderBottomColor : "#e05e3f"
+  },
+  header : {
+    flexDirection : "row",
+    justifyContent : "space-between",
+    alignItems : "center",
+    backgroundColor : "#e05e3f",
+    paddingLeft : 20,
+    padding : 15
   }
 }
 
