f8c5caaf87b6e607e42436b50e59f9e79a4da7d5
CoffeeYes <culinecro@gmail.com>
2020-12-05 13:15:32 +0100

    add files for native bluetooth module

---
+++ android/app/src/main/java/com/herd/BluetoothPackage.kt
@@ -0,0 +1,18 @@
+package com.herd
+
+import com.facebook.react.ReactPackage
+import com.facebook.react.bridge.NativeModule
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.uimanager.ViewManager
+
+import java.util.Collections;
+
+class BluetoothPackage : ReactPackage {
+    override fun createNativeModules(reactContext: ReactApplicationContext): List<NativeModule> {
+        return listOf(BluetoothModule(reactContext))
+    }
+
+    override fun createViewManagers(reactContext: ReactApplicationContext): List<ViewManager<*, *>> {
+        return Collections.emptyList();
+    }
+}
