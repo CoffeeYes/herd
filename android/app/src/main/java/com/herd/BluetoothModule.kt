a2c9b025638a0db84d7f3ea1c3f7fcd0fc54c551
CoffeeYes <culinecro@gmail.com>
2020-12-07 17:59:55 +0100

    throw exceptions on BT adapter errors, add boilerplate for profileListeners

--- android/app/src/main/java/com/herd/BluetoothModule.kt
+++ android/app/src/main/java/com/herd/BluetoothModule.kt
@@ -7,21 +7,38 @@ import com.facebook.react.bridge.ReactMethod
 import com.facebook.react.bridge.Promise
 
 import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothProfile
 
 class BluetoothModule(reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext) {
     override fun getName(): String {
         return "BluetoothModule"
     }
 
+    private val profileListener = object : BluetoothProfile.ServiceListener {
+
+      override fun onServiceConnected(profile: Int, proxy: BluetoothProfile) {
+
+      }
+
+      override fun onServiceDisconnected(profile: Int) {
+
+      }
+    }
+
     @ReactMethod
     fun scanForDevices(promise : Promise) {
       val adapter : BluetoothAdapter? = BluetoothAdapter.getDefaultAdapter();
 
-      if(adapter !== null) {
-        promise.resolve("Adapter found");
+      if(adapter === null) {
+        throw Exception("No BluetoothAdapter Found")
       }
       else {
-        promise.resolve("No Bluetooth Adapter found");
+        if(adapter.isEnabled()) {
+
+        }
+        else {
+          throw Exception("Bluetooth Adapter is disabled")
+        }
       }
     }
 }
