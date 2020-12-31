abc2768ed2c8d57fd1a03108c50f3f9a562e4752
CoffeeYes <culinecro@gmail.com>
2020-12-31 13:42:08 +0100

    add ability to copy own public key to clipboard for sharing

--- src/views/Settings.js
+++ src/views/Settings.js
@@ -1,10 +1,45 @@
-import React from 'react';
-import { Text } from 'react-native'
+import React, { useState } from 'react';
+import { Text, TouchableOpacity, ScrollView } from 'react-native';
+import { useClipboard } from '@react-native-community/clipboard';
+import Crypto from '../nativeWrapper/Crypto'
 
 const Settings = ({ navigation }) => {
+  const [data, setClipboard] = useClipboard();
+  const [showSnackbar, setShowSnackbar] = useState(false);
+
+  const copyKeyToClipboard = () => {
+    Crypto.loadKeyFromKeystore("herdPersonal").then(key => {
+      setClipboard(key)
+      setShowSnackbar(true);
+      setTimeout(() => {setShowSnackbar(false)},500)
+    })
+  }
+
   return (
-    <Text>Settings</Text>
+    <ScrollView>
+      {showSnackbar &&
+      <Text style={{alignSelf : "center",fontSize : 18, fontWeight : "bold"}}>Copied!</Text>}
+      <TouchableOpacity
+      style={styles.button}
+      onPress={copyKeyToClipboard}>
+        <Text style={styles.buttonText}>Copy your key</Text>
+      </TouchableOpacity>
+    </ScrollView>
   )
+}
+
+const styles ={
+  button : {
+    backgroundColor : "#E86252",
+    padding : 10,
+    alignSelf : "center",
+    marginTop : 10,
+    borderRadius : 5
+  },
+  buttonText : {
+    color : "white",
+    fontWeight : "bold"
+  }
 }
 
 export default Settings;
