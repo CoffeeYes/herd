ee6144ee843754691271f53457f4da318f68c81b
CoffeeYes <culinecro@gmail.com>
2021-01-21 11:52:46 +0100

    Added basic ability to share contacts

--- src/views/Contact.js
+++ src/views/Contact.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, TouchableOpacity, ScrollView, TextInput } from 'react-native';
+import { View, Text, TouchableOpacity, ScrollView, TextInput, Share } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import { useClipboard } from '@react-native-community/clipboard';
 import AsyncStorage from '@react-native-async-storage/async-storage'
@@ -16,6 +16,19 @@ const Contact = ({route, navigation}) => {
       setClipboard(contact.key)
       setShowCopied(true);
       setTimeout(() => setShowCopied(false),500)
+    }
+  }
+
+  const shareContact = async () => {
+    const contacts = JSON.parse(await AsyncStorage.getItem("contacts"));
+    const contact = contacts.find(savedContact => savedContact.name === route.params.username);
+
+    if(contact) {
+      const key = contact.key
+      const shared = await Share.share({
+        title : "I'd like to share my Herd Contact with you!",
+        message : key
+      })
     }
   }
 
@@ -41,7 +54,7 @@ const Contact = ({route, navigation}) => {
           <Text style={styles.buttonText}>Copy Key</Text>
         </TouchableOpacity>
 
-        <TouchableOpacity style={styles.button}>
+        <TouchableOpacity style={styles.button} onPress={shareContact}>
           <Text style={styles.buttonText}>Share Contact</Text>
         </TouchableOpacity>
 
