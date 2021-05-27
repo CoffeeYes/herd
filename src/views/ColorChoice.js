a37b5242b22ec84f8c703e4a114bf8a975af23bb
CoffeeYes <culinecro@gmail.com>
2021-05-27 14:40:49 +0200

    update colorchoice to be open/closable, add 4 colorchoices to customise

--- src/views/ColorChoice.js
+++ src/views/ColorChoice.js
@@ -1,17 +1,33 @@
 import React, { useState } from 'react';
-import { View, Dimensions } from 'react-native';
+import { View, Dimensions, Text, TouchableOpacity } from 'react-native';
 import { ColorPicker, fromHsv, toHsv } from 'react-native-color-picker';
 
-const ColorChoice = ({ style, color, setColor }) => {
+const ColorChoice = ({ title, style, setColor }) => {
+  const [open, setOpen] = useState(false);
 
   return (
+    <>
+    {open ?
+    <>
+    <TouchableOpacity
+    style={styles.tab}
+    onPress={() => setOpen(!open)}>
+      <Text>{title}</Text>
+    </TouchableOpacity>
     <View style={styles.colorPickerContainer}>
       <ColorPicker
-      style={{...styles.colorPicker,...style}}
-      color={() => toHsv(color)}
-      onColorChange={color => setColor(fromHsv(color))}
+        style={{...styles.colorPicker,...style}}
+        onColorChange={color => setColor(fromHsv(color))}
       />
     </View>
+    </>
+    :
+    <TouchableOpacity
+    onPress={() => setOpen(!open)}
+    style={styles.tab}>
+      <Text>{title}</Text>
+    </TouchableOpacity>}
+    </>
   )
 }
 
@@ -24,5 +40,11 @@ const styles = {
   colorPicker : {
     width : Dimensions.get("window").width * 0.8,
     height : Dimensions.get("window").height * 0.5
+  },
+  tab : {
+    alignItems : "center",
+    justifyContent : "center",
+    backgroundColor : "white",
+    paddingVertical : 20
   }
 }
--- src/views/Customise.js
+++ src/views/Customise.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { ScrollView, View, Text, Dimensions } from 'react-native';
 import { ColorPicker, fromHsv, toHsv } from 'react-native-color-picker';
+import ColorChoice from './ColorChoice'
 
 const Customise = ({ }) => {
   const [sentBoxColor, setSentBoxColor] = useState("");
@@ -13,31 +14,38 @@ const Customise = ({ }) => {
       <View style={styles.messagesContainer}>
         <View
         style={{...styles.message,...styles.messageFromYou, backgroundColor : sentBoxColor}}>
-          <Text style={{...styles.messageText, color : sentTextColor}}>Hello</Text>
+          <Text style={{...styles.messageText, color : sentTextColor || "black"}}>Hello</Text>
           <Text style={styles.timestamp}>12:20 - 15.01</Text>
         </View>
 
         <View
         style={{...styles.message,...styles.messageFromOther, backgroundColor : receivedBoxColor}}>
-          <Text style={{...styles.messageText,color : receivedTextColor}}>Goodbye</Text>
+          <Text style={{...styles.messageText,color : receivedTextColor || "black"}}>Goodbye</Text>
           <Text style={styles.timestamp}>12:21 - 15.01</Text>
         </View>
       </View>
 
-      <View style={styles.colorPickerContainer}>
-        <ColorPicker
-        style={styles.colorPicker}
-        onColorChange={color => setSentBoxColor(fromHsv(color))}/>
-      </View>
-      <ColorPicker
-      color={sentTextColor}
-      onColorSelected={color => setSentTextColor(color)}/>
-      <ColorPicker
-      color={receivedBoxColor}
-      onColorSelected={color => setReceivedBoxColor(color)}/>
-      <ColorPicker
-      color={receivedTextColor}
-      onColorSelected={color => setReceivedTextColor(color)}/>
+      <ColorChoice
+        title={"Sent Box Color"}
+        color={sentBoxColor}
+        setColor={setSentBoxColor}
+      />
+      <ColorChoice
+        title={"Sent Text Color"}
+        color={sentTextColor}
+        setColor={setSentTextColor}
+      />
+      <ColorChoice
+        title={"Received Box Color"}
+        color={receivedBoxColor}
+        setColor={setReceivedBoxColor}
+      />
+      <ColorChoice
+        title={"Received Text Color"}
+        color={receivedTextColor}
+        setColor={setReceivedTextColor}
+      />
+
     </ScrollView>
   )
 }
