2ce233bb10b5757025eeaecf9778c6c93d390f88
CoffeeYes <culinecro@gmail.com>
2021-07-09 16:46:45 +0200

    correctly decrypt messages in messageQueue

--- src/views/MessageQueue.js
+++ src/views/MessageQueue.js
@@ -3,6 +3,8 @@ import { View, ScrollView, Text } from 'react-native';
 import Header from './Header';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
+import Crypto from '../nativeWrapper/Crypto';
+
 const MessageQueue = ({}) => {
   const [messages, setMessages] = useState([]);
 
@@ -18,9 +20,19 @@ const MessageQueue = ({}) => {
       const currentMessages = JSON.parse(await AsyncStorage.getItem(contacts[contact].id)).sentCopy;
       allMessages = [...allMessages,...currentMessages];
     }
+
+    for(var message in allMessages) {
+      allMessages[message].text = await Crypto.decryptString(
+        "herdPersonal",
+        Crypto.algorithm.RSA,
+        Crypto.blockMode.ECB,
+        Crypto.padding.OAEP_SHA256_MGF1Padding,
+        allMessages[message].text
+      )
+    }
     setMessages(allMessages);
   }
-  
+
   return (
     <View>
       <Header
