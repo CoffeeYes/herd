0a7e167b65d2154e56313c2555939821205cde84
CoffeeYes <culinecro@gmail.com>
2021-07-13 16:27:06 +0200

    add function to manipulate message color when highlighted

--- src/views/ChatBubble.js
+++ src/views/ChatBubble.js
@@ -1,17 +1,34 @@
 import React, { useState } from 'react';
 import { TouchableOpacity, Text } from 'react-native';
-
+import { fromHsv, toHsv } from 'react-native-color-picker';
 
 const ChatBubble = ({ text, timestamp, messageFrom, customStyle }) => {
   const [highlighted, setHighlighted] = useState(false);
+
+  const invertColor = color => {
+    var temp = toHsv(color);
+    temp.h = 0.3 + (0.1* temp.h);
+    temp.s = 0.3 + (0.1* temp.s);
+    temp.v = 0.3 + (0.1* temp.v);
+    temp.a = 0.3 + (0.1* temp.a);
+    temp = fromHsv(temp);
+    return temp;
+  }
 
   return (
     <TouchableOpacity
     onLongPress={() => setHighlighted(true)}
     style={messageFrom ?
-      {...styles.message,...styles.messageFromYou, backgroundColor : customStyle.sentBoxColor}
+      {...styles.message,...styles.messageFromYou, backgroundColor : highlighted ?
+        invertColor(customStyle.sentBoxColor)
+        :
+        customStyle.sentBoxColor
+      }
       :
-      {...styles.message,...styles.messageFromOther, backgroundColor : customStyle.receivedBoxColor}}>
+      {...styles.message,...styles.messageFromOther, backgroundColor : highlighted ?
+        invertColor(customStyle.sentBoxColor)
+        :
+        customStyle.sentBoxColor}}>
       <Text
       style={{
         ...styles.messageText,
