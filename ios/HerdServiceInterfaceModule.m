55d62980f078301efa59c1a2369df9635c17f82e
CoffeeYes <culinecro@gmail.com>
2022-04-01 12:34:56 +0200

    fix ServiceInterface Null reference for event emitter mounting

--- ios/HerdServiceInterfaceModule.m
+++ ios/HerdServiceInterfaceModule.m
@@ -1,9 +1,15 @@
 #import <Foundation/Foundation.h>
 
 #import "HerdServiceInterfaceModule.h"
+#import <React/RCTLog.h>
 
 @implementation HerdServiceInterfaceModule
 
-RCT_EXPORT_MODULE(ServiceInterface);
+RCT_EXPORT_METHOD(isRunning : (RCTPromiseResolveBlock) resolve
+rejecter: (RCTPromiseRejectBlock) reject {
+  resolve(false);
+})
+
+RCT_EXPORT_MODULE(ServiceInterfaceModule);
 
 @end
