177b65cea56d4694cf491966c14357a0e57f3d9d
CoffeeYes <culinecro@gmail.com>
2022-08-09 19:29:07 +0200

    add basic functionality for creating and retrieving RSA keys

--- ios/HerdCryptoModule.swift
+++ ios/HerdCryptoModule.swift
@@ -4,7 +4,19 @@ import Foundation
 class HerdCryptoModule : NSObject {
   @objc
   func generateRSAKeyPair(_ alias : String, resolve : RCTPromiseResolveBlock, reject : RCTPromiseRejectBlock) {
-      resolve(nil);
+      let attributes : [String : Any] = [
+            kSecAttrKeyType as String : kSecAttrKeyTypeRSA,
+            kSecAttrKeySizeInBits as String : 2048,
+            kSecPrivateKeyAttrs as String: [
+                kSecAttrIsPermanent as String:    true,
+                kSecAttrApplicationTag as String: alias
+            ]
+      ]
+      var error : Unmanaged<CFError>?
+      guard let privateKey = SecKeyCreateRandomKey(attributes as CFDictionary, &error) else {
+          return resolve(false);
+      }
+      resolve(true);
   }
   @objc
   func deleteKeyPair(_ alias : String, resolve : RCTPromiseResolveBlock, reject : RCTPromiseRejectBlock) {
@@ -12,7 +24,20 @@ class HerdCryptoModule : NSObject {
   }
   @objc
   func loadKeyFromKeystore(_ alias : String, resolve : RCTPromiseResolveBlock, reject : RCTPromiseRejectBlock) {
-      resolve(nil);
+    let getquery: [String: Any] = [
+        kSecClass as String: kSecClassKey,
+        kSecAttrApplicationTag as String: alias,
+        kSecAttrKeyType as String: kSecAttrKeyTypeRSA,
+        kSecReturnRef as String: true
+    ]
+    var storedKey: CFTypeRef?
+    let status = SecItemCopyMatching(getquery as CFDictionary, &storedKey)
+    guard status == errSecSuccess else { return resolve("error getting private key") }
+    let privateKey = storedKey as! SecKey
+    let publicKey = SecKeyCopyPublicKey(privateKey);
+    let publicKeyData = SecKeyCopyExternalRepresentation(publicKey!,nil)
+    let finalPublicKeyData = publicKeyData! as Data;
+    resolve(finalPublicKeyData.base64EncodedString());
   }
   @objc
   func encryptString(_ alias : String,
