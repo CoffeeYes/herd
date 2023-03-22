867b7645e8ce03ef704573c08ba516adc0d76c88
CoffeeYes <culinecro@gmail.com>
2023-03-22 10:29:47 +0100

    add basic animated loadingBar for skeleton frames, use it in messageQueue

--- src/views/FoldableMessage.js
+++ src/views/FoldableMessage.js
@@ -1,19 +1,54 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { useSelector } from 'react-redux';
-import { View, Text, TouchableOpacity, Dimensions, ActivityIndicator } from 'react-native';
+import { View, Text, TouchableOpacity, Dimensions, ActivityIndicator, Animated } from 'react-native';
 
 import { palette } from '../assets/palette';
 
 import Crypto from '../nativeWrapper/Crypto';
 
+import LoadingBar from './LoadingBar';
+
 const FoldableMessage = ({open, to = "N/A", from = "N/A", closedTimestamp, text, style, onPress, loading,
                           openTimestamp}) => {
   const customStyle = useSelector(state => state.chatReducer.styles);
+  const loadingViewPosition = useRef(new Animated.Value(0)).current;
+
+  const moveToEnd = () => {
+    return Animated.timing(loadingViewPosition, {
+      toValue: 300,
+      duration: 1000,
+      useNativeDriver : false
+    }).start(moveToBeginning)
+  }
+
+  const moveToBeginning = () => {
+    Animated.timing(loadingViewPosition, {
+      toValue: 0,
+      duration: 1000,
+      useNativeDriver : false
+    }).start(moveToEnd)
+  }
+  useEffect(() => {
+    Animated.loop(
+      moveToEnd()
+      // Animated.timing(loadingViewPosition, {
+      //   toValue: 300,
+      //   duration: 1000,
+      //   useNativeDriver : false
+      // }).start(() => {
+      //   Animated.timing(loadingViewPosition, {
+      //     toValue: 0,
+      //     duration: 1000,
+      //     useNativeDriver : false
+      //   }).start();
+      // })
+    );
+  },[loadingViewPosition])
   return (
     <TouchableOpacity style={{...styles.container, paddingBottom : open ? 0 : 20}} onPress={onPress}>
       <View style={{width : "100%"}}>
         {loading ?
-        <ActivityIndicator size="large" color={palette.primary}/>
+        <LoadingBar/>
         :
         <>
           <View style={styles.messageHeader}>
@@ -55,6 +90,20 @@ const styles = {
     borderRadius : 10,
     elevation : 2
   },
+  loadingContainerView : {
+    width : "90%",
+    alignSelf : "center",
+    height : 10,
+    borderRadius : 5,
+    backgroundColor : palette.grey,
+    overflow : "hidden"
+  },
+  loadingView : {
+    width : "20%",
+    height : "100%",
+    backgroundColor : palette.black,
+    borderRadius : 5
+  }
 }
 
 export default FoldableMessage;
---
+++ src/views/LoadingBar.js
@@ -0,0 +1,60 @@
+import React, { useState, useEffect, useRef } from 'react';
+import { View, Animated } from 'react-native';
+
+import { palette } from '../assets/palette';
+
+const LoadingBar = (containerStyle, loadingBarStyle) => {
+  const loadingViewPosition = useRef(new Animated.Value(0)).current;
+  const [containerWidth, setContainerWidth] = useState(0);
+  const [barWidth, setBarWidth] = useState(0);
+  const [animationStarted, setAnimationStarted] = useState(false);
+
+  const moveToEnd = () => {
+    return Animated.timing(loadingViewPosition, {
+      toValue: containerWidth - barWidth,
+      duration: 500,
+      useNativeDriver : false
+    }).start(moveToBeginning)
+  }
+
+  const moveToBeginning = () => {
+    Animated.timing(loadingViewPosition, {
+      toValue: 0,
+      duration: 500,
+      useNativeDriver : false
+    }).start(moveToEnd)
+  }
+  useEffect(() => {
+    if (containerWidth > 0 && barWidth > 0 && !animationStarted && loadingViewPosition) {
+      setAnimationStarted(true);
+      moveToEnd();
+    }
+  },[containerWidth,barWidth,loadingViewPosition])
+
+  return (
+    <View style={{...styles.loadingContainerView,...containerStyle}} onLayout={e => setContainerWidth(e.nativeEvent.layout.width)}>
+      <Animated.View
+      style={{...styles.loadingView,...loadingBarStyle, marginLeft : loadingViewPosition}}
+      onLayout={e => setBarWidth(e.nativeEvent.layout.width)}/>
+    </View>
+  )
+}
+
+export default LoadingBar
+
+const styles = {
+  loadingContainerView : {
+    width : "90%",
+    alignSelf : "center",
+    height : 10,
+    borderRadius : 5,
+    backgroundColor : palette.grey,
+    overflow : "hidden"
+  },
+  loadingView : {
+    width : "20%",
+    height : "100%",
+    backgroundColor : palette.black,
+    borderRadius : 5
+  }
+}
