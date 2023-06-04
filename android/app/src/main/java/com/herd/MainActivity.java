8fc3964b9c64287fd62ba422159a071ca525ee1d
CoffeeYes <culinecro@gmail.com>
2023-06-04 11:28:53 +0200

    add runtime request for BLUETOOTH_CONNECT at app launch, restrict legacy bluetooth permissions to older android versions

--- android/app/src/main/java/com/herd/MainActivity.java
+++ android/app/src/main/java/com/herd/MainActivity.java
@@ -4,8 +4,37 @@ import com.facebook.react.ReactActivity;
 
 import android.view.WindowManager;
 import android.os.Bundle;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import androidx.core.app.ActivityCompat;
+import android.util.Log;
 
 public class MainActivity extends ReactActivity {
+
+  private static final String TAG = "HerdMainActivity";
+  private static final int BLUETOOTH_CONNECT_PERMISSION_REQUEST_CODE = 1;
+
+  private Bundle instanceBundle;
+
+  @Override
+  public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] granted) {
+    Log.d(TAG,"onRequestPermissionsResult");
+    for(int i = 0; i < permissions.length; i++) {
+      Log.i(TAG,"permission : " + permissions[i] + ", granted : " + granted[i]);
+      if(permissions[i] == Manifest.permission.BLUETOOTH_CONNECT) {
+        if(granted[i] == PackageManager.PERMISSION_GRANTED) {
+          startApp(instanceBundle);
+        }
+        else {
+          ActivityCompat.requestPermissions(
+            this,
+            new String[] { Manifest.permission.BLUETOOTH_CONNECT },
+            BLUETOOTH_CONNECT_PERMISSION_REQUEST_CODE
+          );
+        }
+      }
+    }
+  }
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
@@ -16,10 +45,28 @@ public class MainActivity extends ReactActivity {
     return "herd";
   }
 
-  @Override
-  protected void onCreate(Bundle savedInstanceBundle) {
+  private void startApp(Bundle savedInstanceBundle) {
     super.onCreate(savedInstanceBundle);
     getWindow().setFlags(WindowManager.LayoutParams.FLAG_SECURE,
     WindowManager.LayoutParams.FLAG_SECURE);
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceBundle) {
+    int bluetoothConnectPermission = ActivityCompat.checkSelfPermission(
+      getApplicationContext(),
+      Manifest.permission.BLUETOOTH_CONNECT
+    );
+    if (bluetoothConnectPermission != PackageManager.PERMISSION_GRANTED) {
+      instanceBundle = savedInstanceBundle;
+      ActivityCompat.requestPermissions(
+        this,
+        new String[] { Manifest.permission.BLUETOOTH_CONNECT },
+        BLUETOOTH_CONNECT_PERMISSION_REQUEST_CODE
+      );
+    }
+    else {
+      startApp(savedInstanceBundle);
+    }
   }
 }
