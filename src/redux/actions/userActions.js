c52ee6522233c0c98e7f9cc87fea3bfc55f138e0
CoffeeYes <culinecro@gmail.com>
2023-06-26 09:55:04 +0200

    re-factor password update function to prevent defaulting to updating erasure password

--- src/redux/actions/userActions.js
+++ src/redux/actions/userActions.js
@@ -4,8 +4,19 @@ const setPublicKey = key => ({
 })
 
 const setPassword = (passwordName, hash) => {
+  let type;
+  if(passwordName === "login") {
+    type = "SET_LOGIN_PASSWORD";
+  }
+  else if(passwordName === "erasure") {
+    type = "SET_ERASURE_PASSWORD";
+  }
+  else {
+    throw new Error("Unknown passwordName passed to setPassword function, does not match 'login' or 'erasure'");
+  }
+
   return {
-    type : passwordName === "login" ? "SET_LOGIN_PASSWORD" : "SET_ERASURE_PASSWORD",
+    type : type,
     payload : hash
   }
 }
