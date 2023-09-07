11652fde283eae780bccb9ae5d03521edb19b57f
CoffeeYes <culinecro@gmail.com>
2023-09-07 16:56:25 +0200

    simplify tabBar icon selection, screen adjusted tabBar and icons

--- src/views/Main.js
+++ src/views/Main.js
@@ -9,10 +9,14 @@ import Icon from 'react-native-vector-icons/MaterialIcons';
 import Chats from './Chats';
 import Contacts from './Contacts';
 import Settings from './Settings';
+import { useScreenAdjustedSize } from '../helper';
 
 const Tab = createBottomTabNavigator();
 
 const Main = ({ navigation, route }) => {
+
+    const iconSize = useScreenAdjustedSize(0.07,0.04);
+    const tabBarHeight = useScreenAdjustedSize(0.075,0.15,"height");
 
     return(
       <Tab.Navigator
@@ -22,16 +26,10 @@ const Main = ({ navigation, route }) => {
         tabBarShowLabel : false,
         tabBarInactiveTintColor: palette.black,
         tabBarActiveTintColor: palette.primary,
+        tabBarStyle : {height : tabBarHeight},
+        tabBarIconStyle : {width : iconSize},
         tabBarIcon : ({ color, size }) => {
-          if(route.name === "chats") {
-            return <Icon name="chat" size={size} color={color}/>
-          }
-          else if (route.name === "contacts") {
-            return <Icon name="contacts" size={size} color={color}/>
-          }
-          else if (route.name === "settings") {
-            return <Icon name="settings" size={size} color={color}/>
-          }
+          return <Icon name={route.name === "chats" ? "chat" : route.name} size={iconSize} color={color}/>
         }
       })}>
         <Tab.Screen name="chats" component={Chats} />
