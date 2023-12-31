f7b58d55abef1280402581e33ceb93c76b6df98b
CoffeeYes <culinecro@gmail.com>
2023-12-31 10:35:08 +0100

    show confirmation icon when text is copied from chatbubble

--- src/views/ChatBubble.js
+++ src/views/ChatBubble.js
@@ -26,7 +26,16 @@ const ChatBubble = ({ text, textFontSize, timestamp, messageFrom, customStyle, a
   }
 
   const [clipboard, setClipboard] = useClipboard();
+  const [showCopyConfirmation, setShowCopyConfirmation] = useState(false);
 
+  const handleCopyPress = text => {
+    setClipboard(text);
+    setShowCopyConfirmation(true);
+    setTimeout(() => {
+      setShowCopyConfirmation(false);
+    },500)
+  }
+  
   return (
     <TouchableOpacity
     onLayout={onLayout}
@@ -37,11 +46,11 @@ const ChatBubble = ({ text, textFontSize, timestamp, messageFrom, customStyle, a
     style={boxStyle}>
       <View style={{flexDirection : messageFrom ? "row" : "row-reverse"}}>
         {showCopyButton &&
-        <TouchableOpacity onPress={() => setClipboard(text)} style={{
+        <TouchableOpacity onPress={() => handleCopyPress(text)} style={{
           ...styles.copyButton,
           ...(messageFrom ? {marginRight : 20} : {marginLeft : 20})
         }}>
-          <Icon name="content-copy" size={36} color={palette.white}></Icon >
+          <Icon name={showCopyConfirmation ? "check" : "content-copy"} size={36} color={palette.white}></Icon >
         </TouchableOpacity>}
         <View style={styles.textContainer}>
           <Text
