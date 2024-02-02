f6584e03aeed0b4a7c73ca3c31e03b1aee6c72a2
CoffeeYes <culinecro@gmail.com>
2024-02-02 08:02:01 +0100

    limit lineCount in contact header based on lineHeight and half screen height such that text is never clipped

--- src/views/Contact.js
+++ src/views/Contact.js
@@ -28,9 +28,11 @@ const Contact = ({route, navigation}) => {
   const largeImageWidth = useScreenAdjustedSize(0.8,0.8);
   const largeImageHeight = useScreenAdjustedSize(0.8,0.8,"height");
   const cardIconSize = useScreenAdjustedSize(0.075,0.05) + (customStyle.scaledUIFontSize * 0.2);
+  const halfHeight = useScreenAdjustedSize(0.5, 0.5, "height");
 
   const [expandName, setExpandName] = useState(false);
   const [disableTextTouch, setDisableTextTouch] = useState(false);
+  const [headerLineHeight, setHeaderLineHeight] = useState(1);
 
   const copyKeyToClipboard = () => {
     setClipboard(contact.key)
@@ -49,10 +51,13 @@ const Contact = ({route, navigation}) => {
       <Header
       title={contact.name}
       allowGoBack
-      containerStyle={{maxHeight : "50%"}}
-      onTextLayout={e => setDisableTextTouch(e.nativeEvent.lines.length <= 1)}
+      containerStyle={{maxHeight : halfHeight}}
+      onTextLayout={e => {
+        setHeaderLineHeight(e.nativeEvent.lines[0].height)
+        setDisableTextTouch(e.nativeEvent.lines.length <= 1)
+      }}
       disableTextTouch={disableTextTouch}
-      titleNumberOfLines={expandName ? 6 : 1}
+      titleNumberOfLines={expandName ? Math.floor(halfHeight / headerLineHeight) : 1}
       rightButtonIcon="edit"
       onTextTouch={() => setExpandName(!expandName)}
       rightButtonOnClick={() => navigation.navigate("editContact", {id : route.params.id})}/>
