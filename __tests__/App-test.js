26d0cd8bca08ce02ff027b2029106db95fb63fcc
CoffeeYes <culinecro@gmail.com>
2025-03-29 10:18:35 +0100

    add mocks and setup files to allow basic jest tests to run

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -6,9 +6,21 @@ import 'react-native';
 import React from 'react';
 import App from '../App';
 
+import store from '../src/redux/store';
+import { Provider } from 'react-redux'
+
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
+jest.mock('react-native-vector-Icons/MaterialIcons', () => 'Icon');
+jest.mock('react-native-qrcode-svg', () => 'QRCODE');
+jest.mock('react-native-qrcode-scanner', () => 'QRCODE_SCANNER');
+jest.mock('react-native-image-picker', () => 'IMAGE_PICKER');
+
 it('renders correctly', () => {
-  renderer.create(<App />);
+  renderer.create(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
 });
---
+++ mocks/jestSetups/react-native-clipboard-setup.js
@@ -0,0 +1,4 @@
+
+jest.mock('@react-native-clipboard/clipboard', () =>
+  require('@react-native-clipboard/clipboard/jest/clipboard-mock')
+);
