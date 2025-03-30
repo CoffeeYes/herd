e42133ae9e93b62a84264045faa9aa9cb9fb3ecc
CoffeeYes <culinecro@gmail.com>
2025-03-30 09:58:27 +0200

    fix issue with realm handles not closing in tests, causing them to hang indefinitely

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -8,9 +8,12 @@ import App from '../App';
 
 import store from '../src/redux/store';
 import { Provider } from 'react-redux'
+import { closeChatRealm } from '../src/realm/chatRealm';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
+import { closeContactRealm } from '../src/realm/contactRealm';
+import { closePasswordRealm } from '../src/realm/passwordRealm';
 
 jest.mock('react-native-vector-Icons/MaterialIcons', () => 'Icon');
 jest.mock('react-native-qrcode-svg', () => 'QRCODE');
@@ -24,3 +27,10 @@ it('renders correctly', () => {
     </Provider>
   );
 });
+
+afterAll(done => {
+  closeChatRealm();
+  closeContactRealm();
+  closePasswordRealm();
+  done();
+})
