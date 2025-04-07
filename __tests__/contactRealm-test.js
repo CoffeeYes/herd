8af9be3a2d68523abc1c7eb3507246bf31ce21b8
CoffeeYes <culinecro@gmail.com>
2025-04-07 10:34:40 +0200

    add more tests for contactRealm test, revert default test npm script as new open/close realm logic prevents hanging handles

--- __tests__/contactRealm-test.js
+++ __tests__/contactRealm-test.js
@@ -5,9 +5,13 @@ import {
   getAllContacts,
   deleteContacts, 
   closeContactRealm,
-  deleteContactRealm} from '../src/realm/contactRealm.js';
+  deleteContactRealm,
+  getContactById,
+  editContact,
+  getContactsByKey,
+  deleteAllContacts} from '../src/realm/contactRealm.js';
 
-const testContact = {
+const testContact1 = {
   name : "test",
   key : `MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA6Aj7TtjaaostNXW4lwLMeE0I48PoNGKl
         vnQPMRF8TiJ3U5UOeWE5huUYOSnnEzc1LAuh3cuzjtmMB7BbJzsCGV9ZpeKWGFuEqLOFh6/HIYX+
@@ -15,6 +19,17 @@ const testContact = {
         rfR8DiQC4ObO8A7VZlUWHtp0VEpKbnKsXKL9picFfrOJ/HjFZQnmGcbaM1PPazHSvPmJsLB8lHNf
         HauZVHmf4ykKKYt2lzkI4KLZ1RV5rmVXd+XNM9PIKgT5cfnaZViQNsIemcYeljVX/G9qVT8QJ9Kf
         fox+nQIDAQAB`,
+  image : ""
+}
+
+const testContact2 = {
+  name : "test2",
+  key : `MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA6Aj7TtjaaostNXW4lwLMeE0I48PoNGKl
+        vnQPMRF8TiJ3U5UOeWE5huUYOSnnEzc1LAuh3cuzjtmMB7BbJzsCGV9ZpeKWGFuEqLOFh6/HIYX+
+        tzU7IJZc8Hede5BvoyUonzsOsvehCKVlwYlRQdCvsFsrPPsm4qGsMVhbB4ObCA/ELscy8SaA2mc9
+        rfR8DiQC4ObO8A7VZlUWHtp0VEpKbnKsXKL9picFfrOJ/HjFZQnmGcbaM1PPazHSvPmJsLB8lHNf
+        HauZVHmf4ykKKYt2lzkI4KLZ1RV5rmVXd+XNM9PIKgT5cfnaZViQNsIemcYeljVX/G9qVT8QJ9Kf
+        fox+nQIDAQAL`,
   image : ""
 }
 
@@ -30,18 +45,18 @@ afterEach(() => {
   deleteChatRealm();
 })
 
-it("can create contact", () => {
-  createContact(testContact);
+it("create contact", () => {
+  createContact(testContact1);
   const contacts = getAllContacts();
   expect(contacts.length).toBe(1);
-  expect(contacts[0].name).toBe(testContact.name);
-  expect(contacts[0].key).toBe(testContact.key);
-  expect(contacts[0].image).toBe(testContact.image);
+  expect(contacts[0].name).toBe(testContact1.name);
+  expect(contacts[0].key).toBe(testContact1.key);
+  expect(contacts[0].image).toBe(testContact1.image);
   expect(contacts[0]._id).toBeDefined();
 })
 
-it("can delete a single contact, entire object", () => {
-  const contact = createContact(testContact);
+it("delete a single contact", () => {
+  const contact = createContact(testContact1);
   let contacts = getAllContacts();
   expect(contacts.length).toBe(1);
   deleteContacts([contact]);
@@ -49,3 +64,74 @@ it("can delete a single contact, entire object", () => {
   expect(contacts.length).toBe(0);
 })
 
+it("create multiple contacts",() => {
+  createContact(testContact1);
+  createContact(testContact2);
+  const contacts = getAllContacts();
+  expect(contacts.length).toBe(2);
+})
+
+it("delete multiple contacts",() => {
+  const contact1 = createContact(testContact1);
+  const contact2 = createContact(testContact2);
+  let contacts = getAllContacts();
+  expect(contacts.length).toBe(2);
+  deleteContacts([contact1,contact2]);
+  contacts = getAllContacts();
+  expect(contacts.length).toBe(0);
+})
+
+it("delete correct contact out of multiple", () => {
+  const contact1 = createContact(testContact1);
+  const contact2 = createContact(testContact2);
+  let contacts = getAllContacts();
+  expect(contacts.length).toBe(2);
+  deleteContacts([contact2]);
+  contacts = getAllContacts();
+  expect(contacts.length).toBe(1);
+  expect(contacts[0]).toEqual(contact1);
+})
+
+it("delete all contacts", () => {
+  createContact(testContact1);
+  createContact(testContact2);
+  let contacts = getAllContacts();
+  expect(contacts.length).toBe(2);
+  deleteAllContacts();
+  contacts = getAllContacts();
+  expect(contacts.length).toBe(0);
+})
+
+it("retrieve contact by ID", () => {
+  const contact = createContact(testContact1);
+  const retrievedContact = getContactById(contact._id);
+  expect(retrievedContact).toBeDefined();
+  expect(retrievedContact.name).toEqual(contact.name);
+  expect(retrievedContact.key).toEqual(contact.key);
+  expect(retrievedContact.image).toEqual(contact.image);
+})
+
+it("retrieve contact by key", () => {
+  const contact = createContact(testContact1);
+  const retrievedContact = getContactsByKey([contact.key]);
+  expect(retrievedContact.length).toBe(1);
+  expect(retrievedContact[0].name).toEqual(contact.name);
+  expect(retrievedContact[0].key).toEqual(contact.key);
+  expect(retrievedContact[0].image).toEqual(contact.image);
+})
+
+it("retrieve multiple contacts by key", () => {
+  const contact1 = createContact(testContact1);
+  const contact2 = createContact(testContact2);
+  const retrievedContacts = getContactsByKey([contact1.key, contact2.key]);
+  expect(retrievedContacts.length).toBe(2);
+})
+
+it("update a contact", async () => {
+  const {_id} = createContact(testContact1);
+  await editContact({_id, name : "testing", image : "testing", key : testContact2.key})
+  const retrievedContact = getContactById(_id);
+  expect(retrievedContact.name).toBe("testing");
+  expect(retrievedContact.image).toBe("testing");
+  expect(retrievedContact.key).toBe(testContact2.key);
+})
