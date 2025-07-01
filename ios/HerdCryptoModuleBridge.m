c917bf7d71c97a123defba4da6784dbcea90c974
CoffeeYes <culinecro@gmail.com>
2025-07-01 18:08:03 +0200

    remove old encrypt/decrypt functions from IOS native module

--- ios/HerdCryptoModuleBridge.m
+++ ios/HerdCryptoModuleBridge.m
@@ -16,30 +16,6 @@ RCT_EXTERN_METHOD(loadKeyFromKeystore : (NSString*) alias
 resolve : (RCTPromiseResolveBlock)resolve
 reject : (RCTPromiseRejectBlock)reject)
 
-RCT_EXTERN_METHOD(encryptString : (NSString*) alias
-algorithm : (NSString*) algorithm
-blockmode : (NSString*) blockmode
-padding : (NSString*) padding
-stringToEncrypt : (NSString*) stringToEncrypt
-resolve : (RCTPromiseResolveBlock)resolve
-reject : (RCTPromiseRejectBlock)reject)
-
-RCT_EXTERN_METHOD(encryptStringWithKey : (NSString*) key
-algorithm : (NSString*) algorithm
-blockmode : (NSString*) blockmode
-padding : (NSString*) padding
-stringToEncrypt : (NSString*) stringToEncrypt
-resolve : (RCTPromiseResolveBlock)resolve
-reject : (RCTPromiseRejectBlock)reject)
-
-RCT_EXTERN_METHOD(decryptString : (NSString*) alias
-algorithm : (NSString*) algorithm
-blockmode : (NSString*) blockmode
-padding : (NSString*) padding
-stringToDecrypt : (NSString*) stringToDecrypt
-resolve : (RCTPromiseResolveBlock)resolve
-reject : (RCTPromiseRejectBlock)reject)
-
 RCT_EXTERN_METHOD(
 	encryptStrings : (NSString*) keyOrAlias
 	loadKeyFromKeystore : (BOOL) loadKeyFromKeystore
