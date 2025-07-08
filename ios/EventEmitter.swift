4f30f8037cc8fc7f629bde46c28d04bc8d0ca1e4
CoffeeYes <culinecro@gmail.com>
2025-07-08 16:45:52 +0200

    use singleton pattern for IOS EventEmitter instance to prevent app from crashing when attempting to make use of it

--- ios/EventEmitter.swift
+++ ios/EventEmitter.swift
@@ -4,6 +4,13 @@ import React
 @objc(EventEmitter)
 class EventEmitter : RCTEventEmitter {
   
+  public static var emitter : EventEmitter?
+  
+  override init() {
+    super.init()
+    EventEmitter.emitter = self
+  }
+  
   @objc
   override func supportedEvents() -> [String] {
       return []
