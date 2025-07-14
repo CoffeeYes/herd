e1094eb24a151408d748349617ddc5041eb1fd22
CoffeeYes <culinecro@gmail.com>
2025-07-14 16:45:19 +0200

    IOS use enums for ServiceInterface string constants

--- ios/HerdServiceInterfaceModule.swift
+++ ios/HerdServiceInterfaceModule.swift
@@ -3,27 +3,49 @@ import Foundation
 @objc(HerdServiceInterfaceModule)
 class HerdServiceInterfaceModule : NSObject {
   
+  enum emitterStrings : String {
+    case NEW_MESSAGES_RECEIVED = "newHerdMessagesReceived"
+    case REMOVE_MESSAGES_FROM_QUEUE = "removeMessagesFromQueue"
+    case BLUETOOTH_LOCATION_STATE_CHANGE = "bluetoothOrLocationStateChange"
+  }
+  
+  enum storageStrings : String {
+    case SAVED_MESSAGE_QUEUE = "savedMessageQueue"
+    case SAVED_MESSAGE_QUEUE_SIZES = "savedMessageQueueSizes"
+    case MESSAGES_TO_REMOVE = "messagesToRemove"
+    case MESSAGES_TO_REMOVE_SIZES = "messagesToRemoveSizes"
+  }
+  
+  enum bluetoothErrors : String {
+    case LOCATION_DISABLED,ADAPTER_TURNED_OFF
+  }
+  
+  enum messageTypes : String {
+    case COMPLETED_MESSAGES = "completed"
+    case RECEIVED_MESSAGES = "received"
+  }
+  
   @objc
   func constantsToExport() -> [String : Any] {
     return [
       "emitterStrings" : [
-        "NEW_MESSAGES_RECEIVED" : "newHerdMessagesReceived",
-        "REMOVE_MESSAGES_FROM_QUEUE" : "removeMessagesFromQueue",
-        "BLUETOOTH_LOCATION_STATE_CHANGE" : "bluetoothOrLocationStateChange",
+        "NEW_MESSAGES_RECEIVED" : emitterStrings.NEW_MESSAGES_RECEIVED.rawValue,
+        "REMOVE_MESSAGES_FROM_QUEUE" : emitterStrings.REMOVE_MESSAGES_FROM_QUEUE.rawValue,
+        "BLUETOOTH_LOCATION_STATE_CHANGE" : emitterStrings.BLUETOOTH_LOCATION_STATE_CHANGE.rawValue
       ],
       "storage" : [
-        "SAVED_MESSAGE_QUEUE" : "savedMessageQueue",
-        "SAVED_MESSAGE_QUEUE_SIZES" : "savedMessageQueueSizes",
-        "MESSAGES_TO_REMOVE" : "messagesToRemove",
-        "MESSAGES_TO_REMOVE_SIZES" : "messagesToRemoveSizes"
+        "SAVED_MESSAGE_QUEUE" : storageStrings.SAVED_MESSAGE_QUEUE.rawValue,
+        "SAVED_MESSAGE_QUEUE_SIZES" : storageStrings.SAVED_MESSAGE_QUEUE_SIZES.rawValue,
+        "MESSAGES_TO_REMOVE" : storageStrings.MESSAGES_TO_REMOVE.rawValue,
+        "MESSAGES_TO_REMOVE_SIZES" : storageStrings.MESSAGES_TO_REMOVE_SIZES.rawValue,
       ],
       "bluetoothErrors" : [
-        "LOCATION_DISABLED" : "LOCATION_DISABLED",
-        "ADAPTER_TURNED_OFF" : "ADAPTER_TURNED_OFF",
+        "LOCATION_DISABLED" : bluetoothErrors.LOCATION_DISABLED.rawValue,
+        "ADAPTER_TURNED_OFF" : bluetoothErrors.ADAPTER_TURNED_OFF.rawValue,
       ],
       "messageTypes" : [
-        "COMPLETED_MESSAGES" : "completed",
-        "RECEIVED_MESSAGES" : "received"
+        "COMPLETED_MESSAGES" : messageTypes.COMPLETED_MESSAGES.rawValue,
+        "RECEIVED_MESSAGES" : messageTypes.RECEIVED_MESSAGES.rawValue
       ],
     ]
   }
