be3546e2e75cce990c3f3e265cc7b8848c857ee4
CoffeeYes <culinecro@gmail.com>
2025-09-04 18:02:54 +0200

    add BLE UUIDs from android to IOS constants file

---
+++ ios/Constants.swift
@@ -0,0 +1,10 @@
+
+struct bleUUIDs {
+  static let serviceUUID = "30895318-6f7e-4f68-b21a-01a4e2f946fa";
+  static let messageQueueServiceUUID = "7a38fab9-c286-402d-ac6d-6b79c1cbf329";
+  static let messageQueueCharacteristicUUID = "bb301a02-c16e-4c5c-8778-0eb692b132a7";
+  static let messageQueueDescriptorUUID = "663bb186-30a5-4e78-8d42-e8f1a57c5f0a";
+  static let transferCompleteCharacteristicUUID = "547d9721-b143-4113-910b-d0608a3c4fda";
+  static let MTUCharacteristicUUID = "7f9f74f7-2d7d-4f6a-b5c9-81983202ef1f";
+  static let BTConnectionUUID = "acc99392-7f38-11eb-9439-0242ac13000";
+}
